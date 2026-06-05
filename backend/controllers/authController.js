f8d1d6142b8fffe68c1152f89c4f194531d39af7
Hardik <hardikjangra2003@gmail.com>
2026-06-05 12:10:30 +0530

    Setup backend architecture and authentication

---
+++ backend/controllers/authController.js
@@ -0,0 +1,18 @@
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  res.json({
+    message: "Register API Working",
+  });
+};
+
+const loginUser = async (req, res) => {
+  res.json({
+    message: "Login API Working",
+  });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
